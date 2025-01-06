94aa120e4b222d8fbc558e77077864745d364b1f
tarun didwaniya <tarundidwaniya4@gmail.com>
2025-01-06 10:46:24 +0530

    added contect page in app

--- src/Components/Contect/Contect.jsx
+++ src/Components/Contect/Contect.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Stack, Container, Typography, Button } from '../index'
+import SendIcon from '@mui/icons-material/Send';
 
-function Contect() {
+export default function Contect() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Container
+      maxWidth={'md'}
+      sx={{
+        mt: 6
+      }}
+    >
+      <Box
+      >
+        <Typography
+          variant='h3'
+          component={'h1'}
+        >
+          CONTECT US
+        </Typography>
+      </Box>
+      <Box
+        component="form"
+        sx={
+          { '& .MuiTextField-root': { my: 2, width: '100%', display: 'flex' } }}
+        noValidate
+        autoComplete="off"
+      >
+        <Box>
+          <TextField
+            required
+            id="outlined-required"
+            label="Email"
+            type='email'
+          />
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Discription"
+            multiline
+            maxRows={10}
+            rows={10}
+          />
+          <Button 
+            variant="contained" 
+            endIcon={<SendIcon />}
+            sx={{
+              width: '100%'
+            }}
+          >
+            Send
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
-
-export default Contect
